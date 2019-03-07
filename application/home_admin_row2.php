fd66f0dd10a6eb4a2ab9e3e4690b38978ce6f02b
mistraminayasa <manoyasamistra@gmail.com>
2019-03-06 21:51:41 -0800

    first init for changing

---
+++ application/home_admin_row2.php
@@ -0,0 +1,7 @@
+<section class="col-lg-8 connectedSortable">
+    <?php include "home_grafik.php"; ?>
+</section><!-- /.Left col -->
+
+<section class="col-lg-4 connectedSortable">
+    <?php include "home_quick_email.php"; ?>
+</section><!-- right col -->
---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="pull-right hidden-xs">
+    <b></b>
+</div>
